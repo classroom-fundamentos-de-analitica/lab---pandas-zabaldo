d2047ab9a24b83f02a15e3765713ef7da2a2ce16
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-17 07:04:49 -0500

    respuestas 1-8

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,7 @@ def pregunta_01():
     40
 
     """
-    return
+    return len(tbl0)
 
 
 def pregunta_02():
@@ -33,7 +33,7 @@ def pregunta_02():
     4
 
     """
-    return
+    return tbl0.shape[1]
 
 
 def pregunta_03():
@@ -50,7 +50,7 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    return
+    return tbl0["_c1"].value_counts().sort_index()
 
 
 def pregunta_04():
@@ -65,7 +65,7 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].mean().sort_index()
 
 
 def pregunta_05():
@@ -82,7 +82,7 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].max().sort_index()
 
 
 def pregunta_06():
@@ -94,7 +94,8 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    return
+    valores_unicos = tbl1['_c4'].str.upper().unique()
+    return sorted(valores_unicos)
 
 
 def pregunta_07():
@@ -110,7 +111,7 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].sum().sort_index()
 
 
 def pregunta_08():
@@ -128,7 +129,9 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    return
+    tbl0['suma'] = tbl0['_c0'] + tbl0['_c2']
+    # tbl0.to_csv("tbl0.tsv", sep="\t", index=False)
+    return tbl0
 
 
 def pregunta_09():
@@ -218,3 +221,5 @@ def pregunta_13():
     Name: _c5b, dtype: int64
     """
     return
+
+pregunta_07()
